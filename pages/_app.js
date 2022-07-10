5393a6c2698f22f5e5291e7c1c5f5fbe4f65f193
Ciprian Alin <ciprian_enita@yahoo.com>
2022-07-10 12:22:28 +0300

    front end complete

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import "../styles/globals.css"
+import { MoralisProvider } from "react-moralis"
+import { NotificationProvider } from "web3uikit"
 
+const APP_ID = "jMiN9HERRgNSrziDKoF9nbKGlHv76zXUhmquFnoi"
+const SERVER_URL = "https://ig1la5qm2jmw.usemoralis.com:2053/server"
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <MoralisProvider initializeOnMount="false" appId={APP_ID} serverUrl={SERVER_URL}>
+            <NotificationProvider>
+                <Component {...pageProps} />
+            </NotificationProvider>
+        </MoralisProvider>
+    )
 }
 
 export default MyApp
